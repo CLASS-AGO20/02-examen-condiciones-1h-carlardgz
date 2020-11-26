8ed501dce6bc8baa6af0440cdf5cdf4c41066a6b
Carla Rodríguez <crodriguez42@ucol.mx>
2020-11-26 16:21:36 -0600

    implemented costoRenta

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@ export default class App {
       return pesos * 22.7;
     }
   }
+  costoRenta(kilometros) {
+    if (kilometros < 50) {
+      return kilometros * 3;
+    } else if (kilometros >= 50 && kilometros < 100) {
+      return kilometros * 5;
+    } else if (kilometros >= 100 && kilometros < 200) {
+      return kilometros * 6;
+    } else if (kilometros >= 200) {
+      return kilometros * 6.5;
+    }
+  }
 }
 
 let app = new App();
@@ -14,3 +25,9 @@ console.log("Prueba convertirPesos()");
 console.log(app.convertir(8632));
 console.log(app.convertir(568));
 console.log(app.convertir(1));
+
+console.log("Prueba costoRenta()");
+console.log(app.costoRenta(40));
+console.log(app.costoRenta(70));
+console.log(app.costoRenta(180));
+console.log(app.costoRenta(220));
