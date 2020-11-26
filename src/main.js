3defedc0139fecab5a1412913a1ff2062a443d84
Carla Rodríguez <crodriguez42@ucol.mx>
2020-11-26 16:49:36 -0600

    implemented puedeCircular

--- src/main.js
+++ src/main.js
@@ -17,6 +17,91 @@ export default class App {
       return kilometros * 6.5;
     }
   }
+
+  puedeCircular(dia, terminacion) {
+    switch (dia) {
+      case 1:
+        if (terminacion == 0) {
+          return true;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return false;
+        } else {
+          return true;
+        }
+        break;
+      case 2:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return true;
+        } else if (terminacion == 2) {
+          return false;
+        } else {
+          return true;
+        }
+        break;
+      case 3:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return true;
+        } else {
+          return true;
+        }
+        break;
+      case 4:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return true;
+        } else {
+          return true;
+        }
+        break;
+      case 5:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return false;
+        } else {
+          return true;
+        }
+        break;
+      case 6:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return false;
+        } else {
+          return true;
+        }
+        break;
+      case 7:
+        if (terminacion == 0) {
+          return false;
+        } else if (terminacion == 1) {
+          return false;
+        } else if (terminacion == 2) {
+          return false;
+        } else {
+          return true;
+        }
+        break;
+      default:
+        return false;
+        break;
+    }
+  }
 }
 
 let app = new App();
@@ -31,3 +116,19 @@ console.log(app.costoRenta(40));
 console.log(app.costoRenta(70));
 console.log(app.costoRenta(180));
 console.log(app.costoRenta(220));
+
+console.log("Prueba puedeCircular");
+console.log(app.puedeCircular(1, 0));
+console.log(app.puedeCircular(1, 4));
+console.log(app.puedeCircular(2, 1));
+console.log(app.puedeCircular(2, 2));
+console.log(app.puedeCircular(3, 2));
+console.log(app.puedeCircular(3, 4));
+console.log(app.puedeCircular(4, 2));
+console.log(app.puedeCircular(4, 4));
+console.log(app.puedeCircular(5, 5));
+console.log(app.puedeCircular(5, 4));
+console.log(app.puedeCircular(6, 6));
+console.log(app.puedeCircular(6, 4));
+console.log(app.puedeCircular(7, 7));
+console.log(app.puedeCircular(7, 4));
