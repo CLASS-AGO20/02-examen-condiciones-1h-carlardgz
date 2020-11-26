5a5b4215f3d4a73d249ab38b33b3acd05ab3a920
Carla Rodríguez <crodriguez42@ucol.mx>
2020-11-26 16:16:45 -0600

    implemented convertir

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 export default class App {
-
+  convertir(pesos) {
+    if (pesos < 2000) {
+      return pesos * 20.5;
+    } else if (pesos >= 2000) {
+      return pesos * 22.7;
+    }
+  }
 }
 
 let app = new App();
+
+console.log("Prueba convertirPesos()");
+console.log(app.convertir(8632));
+console.log(app.convertir(568));
+console.log(app.convertir(1));
